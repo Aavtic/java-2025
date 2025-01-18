3df18ee0b3b1647a4a85a3fdb126602d5eae8ba7
Aavtic <aadishm79@gmail.com>
2025-01-19 02:47:13 +0530

    Initial Length program

---
+++ ArraysPackageMethods/LengthOfarray.java
@@ -0,0 +1,10 @@
+//# 1. Find the length of the Array variable.
+
+
+class Main {
+    public static void main(String args[]) {
+        int arr[] = new int[5];
+        System.out.println("Length of array: " + arr.length);
+    }
+}
+
