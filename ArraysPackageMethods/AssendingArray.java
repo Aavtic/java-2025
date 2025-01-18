4efce3bd47fc5c10603d5e699441c36722ce9ec4
Aavtic <aadishm79@gmail.com>
2025-01-19 03:09:16 +0530

    Sort Array

---
+++ ArraysPackageMethods/AssendingArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 3, 2, 1};
+        Arrays.sort(arr);
+        System.out.println("Assending Array: : " + Arrays.toString(arr));
+    }
+}
