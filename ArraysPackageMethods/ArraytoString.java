979ff82a3a6d69ddc0e7ca0a915de0f13f90b17f
Aavtic <aadishm79@gmail.com>
2025-01-19 02:53:11 +0530

    Array to String

---
+++ ArraysPackageMethods/ArraytoString.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 3, 4};
+        String arr_str = Arrays.toString(arr);
+
+        System.out.println("Array as string: " + arr_str);
+    }
+}
